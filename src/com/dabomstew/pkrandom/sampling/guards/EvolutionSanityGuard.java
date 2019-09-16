e4053e062e1b8908acfe95e8dafb1dd972e05945
Frederic Kehrein <frederic@kehrein.org>
2019-09-16 19:33:48 +0200

    Adding a probability to sample non-leveling evolution

--- src/com/dabomstew/pkrandom/sampling/guards/EvolutionSanityGuard.java
+++ src/com/dabomstew/pkrandom/sampling/guards/EvolutionSanityGuard.java
@@ -9,12 +9,30 @@ public final class EvolutionSanityGuard extends EncounterGuard implements BasePr
         // Basis pokemon -> Lvl 0 base line
         if (pkmn.evolutionsTo.isEmpty()) return 0;
         // We want to find out the lowest level at which we can get this pokemon by evolution
-        // if the pokemon doesn't evolve by level, we are simply going going to return infinity (200, should be high enough)
+        // if the pokemon doesn't evolve by level, we try to do a "smart" estimate
         int result = 200;
         for (Evolution priorEvo: pkmn.evolutionsTo) {
+            int evoLevel = 200;
             if (priorEvo.type.usesLevel()) {
-                int evoLevel = priorEvo.extraInfo;
+                evoLevel = priorEvo.extraInfo;
                 if (evoLevel < result) result = evoLevel;
+            } else { // e.g. Stone evolutions
+                evoLevel = getUpperBound(pkmn);
+                // Check if there is another evolution
+                // e.g. Slowpoke can also become Slowbro
+                // so we assume the same level for both evos
+                if (evoLevel == 100) {
+                    // There is no levelup evo
+                    // Check the evo level of prior step
+                    int priorEvoLevel = getLowerBound(priorEvo.from);
+                    // now we just say the evolution level is the prior +10
+                    // but for Base 2 evos we don't want them that early
+                    // So at least lvl 25 is required
+                    evoLevel = Math.max(priorEvoLevel + 10, 25);
+                }
+            }
+            if (evoLevel < result) {
+                result = evoLevel;
             }
         }
         return result;
@@ -27,8 +45,12 @@ public final class EvolutionSanityGuard extends EncounterGuard implements BasePr
                 int evoLevel = nextEvo.extraInfo;
                 if (evoLevel > result) result = evoLevel;
             }
+            // As most stone evolutions don't learn any new moves after
+            // evolving,
+            // We don't wan't a max level for encountering it's prior form
+            // Also this behavior is required by getLowerBound()
         }
-        
+
         // minus 1 as we want the max level before evolving
         return result == 0 ? 100 : result - 1;
     }
