a4adf4f05f205d40c6ca0c61380ba25fc12dcf49
Ajarmar <axel.jarmar@gmail.com>
2020-06-18 15:41:27 +0200

    update list of legendaries/strong legendaries

--- src/com/dabomstew/pkrandom/pokemon/Pokemon.java
+++ src/com/dabomstew/pkrandom/pokemon/Pokemon.java
@@ -222,10 +222,11 @@ public class Pokemon implements Comparable<Pokemon> {
 
     private static final List<Integer> legendaries = Arrays.asList(144, 145, 146, 150, 151, 243, 244, 245, 249, 250,
             251, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488,
-            489, 490, 491, 492, 493, 494, 638, 639, 640, 641, 642, 643, 644, 645, 646, 647, 648, 649);
+            489, 490, 491, 492, 493, 494, 638, 639, 640, 641, 642, 643, 644, 645, 646, 647, 648, 649, 716, 717, 718,
+            719, 720, 721, 785, 786, 787, 788, 789, 790, 791, 792, 800, 801, 802, 807);
 
     private static final List<Integer> strongLegendaries = Arrays.asList(150, 249, 250, 382, 383, 384, 483, 484, 486,
-            487, 493, 643, 644, 646);
+            487, 493, 643, 644, 646, 716, 717, 791, 792);
 
     public boolean isLegendary() {
         return formeNumber == 0 ? legendaries.contains(this.number) : legendaries.contains(this.baseForme);
