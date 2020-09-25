b5ce24dde6ce527e49ef7128101b4c73144c2f8d
Ajarmar <axel.jarmar@gmail.com>
2020-09-25 18:09:29 +0200

    update version number to release version

--- src/com/dabomstew/pkrandom/Version.java
+++ src/com/dabomstew/pkrandom/Version.java
@@ -28,8 +28,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Version {
-    public static final int VERSION = 311; // Increment by 1 for new version. Updated for 4.0.0-dev
-    public static final String VERSION_STRING = "4.0.0-dev";
+    public static final int VERSION = 311; // Increment by 1 for new version. Updated for 4.0.0
+    public static final String VERSION_STRING = "4.0.0";
 
     public static final Map<Integer,String> oldVersions = setupVersionsMap();
 
