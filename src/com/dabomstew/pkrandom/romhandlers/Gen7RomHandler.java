5d90704e87b6ff1d2980b0e2671832b55a62c097
tom-overton <tom.overton@outlook.com>
2020-10-19 11:02:28 -0700

    Gen 7: Allow Minior-C in wilds (with code patch to make them work)

--- src/com/dabomstew/pkrandom/romhandlers/Gen7RomHandler.java
+++ src/com/dabomstew/pkrandom/romhandlers/Gen7RomHandler.java
@@ -1196,6 +1196,7 @@ public class Gen7RomHandler extends Abstract3DSRomHandler {
 
         try {
             saveAreaData();
+            patchMiniorEncounterCode();
         } catch (IOException e) {
             throw new RandomizerIOException(e);
         }
@@ -1346,15 +1347,15 @@ public class Gen7RomHandler extends Abstract3DSRomHandler {
         return String.join(" / ", uniqueZoneNames);
     }
 
-    @Override
-    public List<Pokemon> bannedForWildEncounters() {
-        // Ban Minior-C from appearing in the wild because it just spawns regular Minior.
-        int miniorCoreStartingIndex = Gen7Constants.getMiniorCoreStartingIndex(romEntry.romType);
-        List<Pokemon> miniorCoreList = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
-            miniorCoreList.add(pokes[miniorCoreStartingIndex + i]);
+    private void patchMiniorEncounterCode() {
+        int offset = find(code, Gen7Constants.miniorWildEncounterPatchPrefix);
+        if (offset > 0) {
+            offset += Gen7Constants.miniorWildEncounterPatchPrefix.length() / 2;
+            code[offset] = 0x00;
+            code[offset + 1] = 0x00;
+            code[offset + 2] = 0x00;
+            code[offset + 3] = 0x00;
         }
-        return miniorCoreList;
     }
 
     @Override
