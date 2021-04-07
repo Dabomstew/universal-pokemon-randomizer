af56db46ca3f35d8840dede719bfd8c01ddd43f4
Ajarmar <axel.jarmar@gmail.com>
2021-04-07 17:25:42 +0200

    launcher: add launcher scripts

---
+++ launcher/launcher_UNIX.sh
@@ -0,0 +1,4 @@
+cd "$( dirname $0 )"
+java -Xmx4096M -jar PokeRandoZX.jar please-use-the-launcher
+echo "Press Enter to exit..."
+read -s
---
+++ launcher/launcher_WINDOWS.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "%~dp0"
+java -Xmx4096M -jar PokeRandoZX.jar please-use-the-launcher
+echo Press any key to exit...
+pause >nul
